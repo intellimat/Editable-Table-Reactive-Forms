bd34782c53bd06ce2f9102bd5adf186348c8c779
Matteo <matnice23@gmail.com>
2022-07-01 09:57:39 +0200

    boilerplate_code

---
+++ app/src/app/features/features.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { UsersTableComponent } from './components/users-table/users-table.component';
+
+@NgModule({
+  declarations: [UsersTableComponent],
+  imports: [CommonModule],
+  exports: [UsersTableComponent],
+})
+export class FeaturesModule {}
