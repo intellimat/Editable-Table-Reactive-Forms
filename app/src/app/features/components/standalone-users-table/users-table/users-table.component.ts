220207d06bfebd2c48b50c8b36da1a165b4931db
Matteo <matnice23@gmail.com>
2022-07-02 16:46:23 +0200

    sketched alt solution

--- app/src/app/features/components/standalone-users-table/users-table/users-table.component.ts
+++ app/src/app/features/components/standalone-users-table/users-table/users-table.component.ts
@@ -1,15 +1,61 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnChanges } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Department, User } from 'src/app/models/user.model';
 
 @Component({
   selector: 'app-users-table',
   templateUrl: './users-table.component.html',
-  styleUrls: ['./users-table.component.scss']
+  styleUrls: ['./users-table.component.scss'],
 })
-export class UsersTableComponent implements OnInit {
+export class UsersTableComponent implements OnChanges {
+  @Input() data!: User[] | null;
+  departments = Object.values(Department);
+  usersTable = this.formBuilder.group({
+    tableRows: this.formBuilder.array([]),
+  });
+  tableRows = this.usersTable.get('tableRows') as FormArray;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) {}
 
-  ngOnInit(): void {
+  ngOnChanges() {
+    this.usersTable = this.formBuilder.group({
+      tableRows: this.formBuilder.array([]),
+    });
+    this.data?.forEach((user) => {
+      this.addRow(user);
+    });
   }
 
+  private createFormGroup(user: User): FormGroup {
+    return this.formBuilder.group({
+      name: [user.name, Validators.required],
+      email: [user.email, [Validators.email, Validators.required]],
+      department: [user.department, [Validators.required]],
+      isEditable: [true],
+    });
+  }
+
+  addRow(user: User) {
+    const tableRows = this.usersTable.get('tableRows') as FormArray;
+    const newUser = this.createFormGroup(user);
+    tableRows.push(newUser);
+  }
+
+  deleteRow(index: number) {
+    const tableRows = this.usersTable.get('tableRows') as FormArray;
+    tableRows.removeAt(index);
+  }
+
+  private setIsEditable(group: FormGroup, value: boolean) {
+    group.get('isEditable')!.setValue(value);
+  }
+
+  saveUserDetails() {
+    console.log(this.usersTable.value);
+  }
+
+  get getFormControls() {
+    const control = this.usersTable.get('tableRows') as FormArray;
+    return control;
+  }
 }
--- app/src/app/features/features.module.ts
+++ app/src/app/features/features.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UsersTableComponent } from './components/standalone-users-table/users-table/users-table.component';
 import { UsersTableContainerComponent } from './components/standalone-users-table/users-table-container/users-table-container.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [UsersTableComponent, UsersTableContainerComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   exports: [UsersTableContainerComponent],
 })
 export class FeaturesModule {}
