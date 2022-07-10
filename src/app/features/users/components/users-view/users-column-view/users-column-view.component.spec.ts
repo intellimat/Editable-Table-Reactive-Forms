88aaf8c1de9cf860f61374191ac80db207d77a13
Matteo <matnice23@gmail.com>
2022-07-10 22:37:48 +0200

    fixed tests

--- src/app/features/users/components/users-view/users-column-view/users-column-view.component.spec.ts
+++ src/app/features/users/components/users-view/users-column-view/users-column-view.component.spec.ts
@@ -1,5 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ReactiveFormsModule } from '@angular/forms';
+import { DepartmentContainerComponent } from './department-container/department-container.component';
 import { UsersColumnViewComponent } from './users-column-view.component';
 
 describe('UsersColumnViewComponent', () => {
@@ -8,9 +9,9 @@ describe('UsersColumnViewComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ UsersColumnViewComponent ]
-    })
-    .compileComponents();
+      declarations: [UsersColumnViewComponent, DepartmentContainerComponent],
+      imports: [ReactiveFormsModule],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(UsersColumnViewComponent);
     component = fixture.componentInstance;
