a7cd87e39628e38ae9e2a4c7e2199ba04b46286d
Matteo <matnice23@gmail.com>
2022-07-11 01:08:36 +0200

    fixed table borders

--- src/app/shared/card/card.component.ts
+++ src/app/shared/card/card.component.ts
@@ -5,13 +5,11 @@ import { Component, Input, OnInit } from '@angular/core';
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.scss'],
 })
-export class CardComponent implements OnInit {
+export class CardComponent {
   @Input() title!: string;
   @Input() subtitle!: string;
   @Input() tag!: string;
   @Input() img: string = '';
 
   constructor() {}
-
-  ngOnInit(): void {}
 }
